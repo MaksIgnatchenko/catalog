34c169155c2768aab29bba59534fb2063f10cad1
maksym.ignatchenko <maksym.ignatchenko@appus.me>
2018-12-20 14:54:17 +0200

    fix adblocks sort

--- app/Modules/Advertisement/Providers/RouteServiceProvider.php
+++ app/Modules/Advertisement/Providers/RouteServiceProvider.php
@@ -7,6 +7,9 @@
 namespace App\Modules\Advertisement\Providers;
 
 use App\Helpers\SubDomain;
+use App\Modules\Advertisement\Models\Adblock;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
@@ -51,5 +54,24 @@ class RouteServiceProvider extends ServiceProvider
         Route::prefix('api')
             ->namespace($this->adminNamespace)
             ->group(__DIR__ . './../Routes/api.php');
+    }
+
+    public function boot()
+    {
+        parent::boot();
+
+        Route::bind('adblock', function ($value) {
+            try {
+                $adblock = Adblock::where('id', (int)$value)->firstOrFail();
+            } catch (ModelNotFoundException $exception) {
+                $exception->setModel('adblock');
+                throw $exception;
+            } catch (QueryException $exception) {
+                $exception = new ModelNotFoundException();
+                $exception->setModel('adblock');
+                throw $exception;
+            }
+            return $adblock;
+        });
     }
 }
