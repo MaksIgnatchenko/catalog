e60e79a25b8ef6856c3ba3409bfc817a3b898875
maksym.ignatchenko <maksym.ignatchenko@appus.me>
2019-01-29 15:38:02 +0200

    fix bug with send verification email

--- app/Modules/Companies/Resources/views/home.blade.php
+++ app/Modules/Companies/Resources/views/home.blade.php
@@ -5,14 +5,6 @@
     <div class="clearfix"></div>
     @include('flash::message')
     <div class="clearfix"></div>
-    <div class="col-md-3 col-sm-4">
-        <div class="pad box-pane-right bg-green"
-             style="min-height: 400px; display: flex;justify-content: center;align-items: center;">
-            <div class="description-block margin-bottom">
-                <h5 class="description-header">Owner</h5>
-                <h3 class="description-text">{{$dto->getCompanyOwnerEmail()}}</h3>
-            </div>
-        </div>
-    </div>
+
 @endsection
 
